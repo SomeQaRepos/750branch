0aff3145a70ff91b2b41613bb6d16e9c0cb6dc0b
birolcaglar97 <birolcaglar97@gmail.com>
2023-03-07 16:03:23 +0300

    committt

--- sa.java
+++ sa.java
@@ -2,7 +2,7 @@ public class sa {
     public static void main(String[] args) {
         for (int i = 0; i < 750; i++) {
             System.out.println("git checkout dropdown" + i);
-            System.out.println("git push");
+            System.out.println("git push --set-upstream origin");
         }
     }
 }
